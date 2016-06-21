687ec8fc6a2b6f6987332fd91bb9e221c82fd2eb
Ricardo Romo Ramírez <ricardo.romo.ramirez@gmail.com>
2016-06-21 18:00:09 -0500

    Support for linting

--- Jakefile.js
+++ Jakefile.js
@@ -1,12 +1,9 @@
-desc("Example!");
-task("default", ["example"], function(){
-  console.log("default");
-});
+"use strict";
 
-task("example", ["dependency"], function(){
-  console.log("example task");
-});
+task("default", ["lint"]);
 
-task("dependency", function(){
-  console.log("dependency");
+desc("Lint everything");
+task("lint", [], function(){
+  var lint = require("./build/util/lint_runner.js");
+  lint.validateFile("Jakefile.js", {}, {});
 });

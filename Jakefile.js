f45536874301fd6017bd3a2694db9163631ebaa0
Ricardo Romo Ramírez <ricardo.romo.ramirez@gmail.com>
2016-06-22 18:18:26 -0500

    Setup strict lint options

--- Jakefile.js
+++ Jakefile.js
@@ -1,18 +1,36 @@
-"use strict";
 /* global desc, task, jake, fail, complete */
-task("default", ["lint"]);
+(function (){
+  "use strict";
+  task("default", ["lint"]);
 
-desc("Lint everything");
-task("lint", [], function(){
-  var lint = require("./build/util/lint_runner.js");
-  var files = new jake.FileList();
-  files.include("**/*.js");
-  files.exclude("node_modules");
-  files.exclude("build");
+  desc("Lint everything");
+  task("lint", [], function(){
+    var lint = require("./build/util/lint_runner.js");
+    var files = new jake.FileList();
+    files.include("**/*.js");
+    files.exclude("node_modules");
 
-  var options = {
-    node: true
-  };
+    var options = nodeLintOptions();
+    lint.validateFileList(files.toArray(), options, {});
+  });
 
-  lint.validateFileList(files.toArray(), options, {});
-});
+  function nodeLintOptions(){
+    return {
+      bitwise: true,
+      curly: false,
+      eqeqeq: true,
+      forin: true,
+      immed: true,
+      latedef: false,
+      newcap: true,
+      noarg: true,
+      noempty: true,
+      nonew: true,
+      regexp: true,
+      undef: true,
+      strict: true,
+      trailing: true,
+      node: true,
+    };
+  }
+}());
